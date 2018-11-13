b58636f3fb2655653dc5e713b63909bbc3ed4e68
Claudia Fox <claudiafox95@icloud.com>
2018-11-13 23:38:34 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -35,7 +35,7 @@ def selective_tweet_shortener(tweet)
   tweet.split(" ").map do |phrase|
     if phrase.length > 140
       word_substituter(phrase)
-    elsif phrase.length < 130
+    elsif phrase.length <= 130
        phrase
     end
   end
