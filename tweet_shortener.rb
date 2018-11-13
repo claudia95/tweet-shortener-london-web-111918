0832afe74de7bb100829d94b02531fa066a3f0fb
Claudia Fox <claudiafox95@icloud.com>
2018-11-13 23:23:08 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,7 +1,13 @@
 # Write your code here.
 def dictionary
   {
-    
+    "hello" => "hi",
+    "to" || "two" || "too" => "2",
+    "for" || "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
   }
 end
 
