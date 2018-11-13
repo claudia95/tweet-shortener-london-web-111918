97d17c859e7625b9888df8e6080531006d27497c
Claudia Fox <claudiafox95@icloud.com>
2018-11-13 23:30:09 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -18,7 +18,7 @@ end
 def word_substituter(tweet)
   tweet.split(" ").map do |word|
     if dictionary.keys.include?(word.downcase)
-      word = dictionary [word.downcase]
+      word = dictionary[word.downcase]
     else
       word
     end
